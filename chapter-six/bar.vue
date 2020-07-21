de767a2700dc1097d030eb9334955f572a5de357
Kentaro Minami <mkentaro555@gmail.com>
2020-07-21 16:44:20 +0900

    スコープ付きCSS

---
+++ chapter-six/bar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bar">
+    <h1 class="header">Barコンポーネント</h1>
+    <p>これはBarコンポーネントです。</p>
+    <!-- Fooコンポーネントの利用。scriotブロックも要注目。 -->
+    <foo class="header" />
+  </div>
+</template>
+    
+<style scoped>
+.bar {
+  border: solid 1px red;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 125%;
+}
+</style>
+
+<script>
+import Foo from "./foo";
+
+export default {
+  // FooコンポーネントをBarコンポーネントに登録
+  components: { Foo }
+};
+</script>
---
+++ chapter-six/root.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="root">
+    <h1 class="header">Rootコンポーネント</h1>
+    <p>これはRootコンポーネントです。</p>
+    <!-- Fooコンポーネントの利用。scriotブロックも要注目。 -->
+    <foo />
+    <bar />
+  </div>
+</template>
+    
+<style scoped>
+#root {
+  border: solid 1px blue;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 200%;
+}
+</style>
+
+<script>
+import Foo from "./foo";
+import Bar from "./bar";
+
+export default {
+  // FooコンポーネントをBarコンポーネントに登録
+  components: { Foo, Bar }
+};
+</script>
